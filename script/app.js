780410e70adecca585bba93c28f4212507c8182b
Marcos Willian <166168504+MarcosWillianA@users.noreply.github.com>
2024-10-11 14:38:23 -0400

    quick save2
    salvando só pra garantir.

--- script/app.js
+++ script/app.js
@@ -6,10 +6,8 @@ const direcional = document.querySelectorAll('.direcional');
 const cima = document.querySelector('#cima');
 const baixo = document.querySelector('#baixo');
 const direita = document.querySelector('#cima');
-const gameOver = document.querySelector('#gameover')
-const recomecar = document.querySelector('#recomecar')
-
-
+const gameOver = document.querySelector('#gameover');
+const recomecar = document.querySelector('#recomecar');
 
 let coordenadasCobra = [];
 
@@ -30,12 +28,36 @@ function criarTabuleiro(linha, coluna) {
 
 criarTabuleiro(16, 16);
 
+let celulas = document.querySelectorAll('.celula');
+
 // Criação do movimento 
 
 function aparecerComida () {
-    let celulas = document.querySelectorAll('.celula');
-    coordCelula = Math.floor(Math.random() * celulas.length);
-    celulas[coordCelula].classList.add('comida');
+    aleatorioComida = Math.floor(Math.random() * celulas.length);
+    console.log(aleatorioComida);
+    celulas[aleatorioComida].classList.add('comida');
 }
 
 aparecerComida();
+
+function aparecerCobra() {
+    const numeros = [129, 130, 131, 132, 145, 146, 147, 148];
+    const indiceAleatorio = Math.floor(Math.random() * numeros.length);
+    const numeroAleatorio = numeros[indiceAleatorio];
+    console.log(numeroAleatorio);
+
+    celulas[numeroAleatorio].classList.add('cobra');
+    let posicaoCobra = numeroAleatorio;
+    return posicaoCobra;
+}
+
+aparecerCobra();
+
+function moverCobra () {
+    console.log(`Posição cobra: ${posicaoCobra}`)
+    setInterval(() => {
+        celulas[posicaoCobra + 16].classList.add('cobra');
+    }, 1000);
+}
+
+moverCobra();
